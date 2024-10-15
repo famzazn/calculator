b5c03890c7821cf1100c51631ef9e60f9e8cdfad
famzazn <famzazn@proton.me>
2024-10-15 18:51:57 -0400

    Create Basic Operator Functions

--- calculator.js
+++ calculator.js
@@ -1,1 +1,20 @@
-console.log("yoyoyoyoyoy");
+const add = function(x, y) {
+    return x + y;
+}
+
+const subtract = function(x, y) {
+    return x - y;
+}
+
+const multiply = function(x, y) {
+    return x * y;
+}
+
+const divide = function(x, y) {
+    return x / y;
+}
+
+console.log(add(5, 7));
+console.log(subtract(5, 7));
+console.log(multiply(5, 7));
+console.log(divide(5, 7));
