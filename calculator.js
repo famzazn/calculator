5b226019acfe189a33e97a44f72b0b3e8f92e4ed
famzazn <famzazn@proton.me>
2024-10-15 23:47:59 -0400

    Add button, display, and fixed operate function logic

--- calculator.js
+++ calculator.js
@@ -14,7 +14,46 @@ const divide = function(x, y) {
     return x / y;
 }
 
-console.log(add(5, 7));
-console.log(subtract(5, 7));
-console.log(multiply(5, 7));
-console.log(divide(5, 7));
+let number1; // link to button 1
+let operator; // link to operator button
+let number2; // link to button 2
+
+const operate = function(number1, operator, number2) {
+    if (operator === '+') {
+        return add(number1, number2);
+    } else if (operator === '-') {
+        return subtract(number1, number2);
+    } else if (operator === '*') {
+        return multiply(number1, number2);
+    } else if (operator === '/') {
+        return divide(number1, number2);
+    }
+
+}
+
+console.log(operate(6, "-", 9))
+
+let displayText = ''
+
+const clear = document.querySelector(".clear");
+clear.addEventListener('click', () => {
+    document.querySelector('.display').textContent = ""
+    displayText = '';
+})
+
+
+
+const btn = document.querySelectorAll('.digits button, .operation button');
+btn.forEach((button) => {
+    button.addEventListener('click', () => {
+        displayText += button.textContent;
+        document.querySelector('.display').textContent = displayText;
+    })
+})
+
+const equal = document.querySelector('.equals')
+equal.addEventListener('click', () => {
+    displayText += '=' + operate()               
+})
+
+//Need to somehow make number input go into operate function
